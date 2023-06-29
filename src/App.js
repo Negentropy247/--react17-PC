a72e1ffb582925fb4097727f6804c7bdbddc9a9f
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-06-29 16:29:28 +0800

    day09路由配好了-路由重定向也做了

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import NotFound from './pages/NotFound'
 
 export default function App() {
-  return <div>根组件--test</div>
+  return (
+    <Router>
+      <div>
+        {/* 渲染组件 */}
+        <Switch>
+          {/* 路由重定向 */}
+          <Redirect exact from="/" to="/home"></Redirect>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/home" component={Layout}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function NotFound() {
+  return <div>快乐星球被我吃了</div>
+}
