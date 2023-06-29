17d5fd3816906650d25b08c23f3a1706a5c75019
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-06-29 22:30:17 +0800

    登录模块--form基本结构（h讲的真简洁明了，听着好清爽）

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Button, Checkbox, Form, Input } from 'antd'
 import logo from '@/assets/logo.png'
 import './index.scss'
 
@@ -7,7 +7,34 @@ export default function Login() {
   return (
     <div className="login">
       <Card className="login-container">
+        {/* 图片渲染 */}
         <img className="login-logo" src={logo} alt="" />
+
+        {/* 表单渲染 */}
+        <Form
+          // onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          size="large"
+        >
+          <Form.Item>
+            <Input placeholder="输入手机号" />
+          </Form.Item>
+
+          <Form.Item>
+            <Input placeholder="输入验证码" />
+          </Form.Item>
+
+          <Form.Item>
+            <Checkbox>你真的超努力超优秀</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              进入年薪25w星球
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
