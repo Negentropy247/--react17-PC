716225badc16454d592f0dacac5861c975416c9e
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-02 15:53:43 +0800

    内容管理--日期中文格式

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import './index.scss'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
+// 默认语言为 en-US，如果你需要设置其他语言，推荐在入口文件全局设置 locale
+import 'moment/locale/zh-cn'
+import locale from 'antd/es/locale/zh_CN'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConfigProvider locale={locale}>
+      <App />
+    </ConfigProvider>
   </Provider>,
   document.getElementById('root')
 )
