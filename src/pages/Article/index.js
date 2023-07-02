6fd0bd3cb7060e1b82b6a1f9c13d9424c9dec0e1
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-02 14:14:09 +0800

    内容管理--面包屑组件

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import styles from './index.module.scss'
+import { Breadcrumb, Card } from 'antd'
+import { Link } from 'react-router-dom'
 
 export default function Article() {
-  return <div>内容管理</div>
+  return (
+    <div className={styles.root}>
+      <Card
+        title={
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to={'/home'}>首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </div>
+  )
 }
