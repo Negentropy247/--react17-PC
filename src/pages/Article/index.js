b3663fad53fefda9d5ac06122a7b6752d3797eea
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-02 15:31:19 +0800

    内容管理--基本结构（能看到博主妻子的变化，虽然是很好的生活，但是我想成为博主）

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './index.module.scss'
-import { Breadcrumb, Card } from 'antd'
+import { Breadcrumb, Button, Card, DatePicker, Form, Radio, Select } from 'antd'
 import { Link } from 'react-router-dom'
 
 export default function Article() {
@@ -16,9 +16,35 @@ export default function Article() {
           </Breadcrumb>
         }
       >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+        {/* 表单 */}
+        <Form initialValues={{ status: -1 }}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={-1}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="频道" name="channel_id">
+            <Select
+              style={{
+                width: 120
+              }}
+              placeholder="请选择频道"
+            >
+              <Select.Option value="1">111</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="日期 ">
+            <DatePicker.RangePicker></DatePicker.RangePicker>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary">筛选</Button>
+          </Form.Item>
+        </Form>
+        {/* 表格 */}
       </Card>
     </div>
   )
