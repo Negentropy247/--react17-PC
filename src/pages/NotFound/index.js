513ae4a0b39b4be9928a2c260ef1ff67c19e5f11
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-04 17:47:05 +0800

    repeat

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
-  return <div>快乐星球被我吃了</div>
+  const [time, setTime] = useState(5)
+  const timeRef = useRef(time)
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime(timeRef.current - 1)
+      if (timeRef.current <= 1) {
+        clearInterval(timer)
+      }
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    timeRef.current = time
+  }, [time])
+  return (
+    <div>
+      快乐星球被我吃了
+      <Link to="/home"> {time}s后自动返回快乐星球</Link>
+    </div>
+  )
 }
