e08b3ed4d2ef9e5480d979e646ab5b2a9007fefa
我们去月球漫步 <13105694+flymetothemoon-wj@user.noreply.gitee.com>
2023-07-05 20:51:29 +0800

    他娘的，不写了，鲨鱼是我的精神支柱！！！大哭特哭！！！

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Card, Breadcrumb, Form, Input, Select, Button, Space } from 'antd'
+import React, { useState } from 'react'
+import { Card, Breadcrumb, Form, Input, Select, Button, Space, Radio, Upload } from 'antd'
 import styles from './index.module.scss'
 import { Link } from 'react-router-dom'
 // import { useChannels } from '@/hooks'
 import Channel from '@/components/channel'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { PlusOutlined } from '@ant-design/icons'
 
 export default function Publish() {
   // const channels = useChannels()
+  const onFinish = value => {}
+  const [fileList, setFileList] = useState([{}])
   return (
     <div className={styles.root}>
       {/* 面包屑 */}
@@ -19,24 +24,45 @@ export default function Publish() {
             <Breadcrumb.Item>发布文章</Breadcrumb.Item>
           </Breadcrumb>
         }
-      ></Card>
-      {/* 表格 */}
-      <Form size="large" labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
-        <Form.Item label="标题">
-          <Input style={{ width: 400 }} placeholder="请输入文章的标题"></Input>
-        </Form.Item>
-        <Form.Item label="频道">
-          <Channel />
-        </Form.Item>
-        <Form.Item label="封面"></Form.Item>
-        <Form.Item label="内容"></Form.Item>
-        <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
-          <Space>
-            <Button type="primary">发布文章</Button>
-            <Button>存入草稿</Button>
-          </Space>
-        </Form.Item>
-      </Form>
+      >
+        <Form
+          initialValues={{ content: '', type: 1 }}
+          onFinish={onFinish}
+          size="large"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 20 }}
+        >
+          <Form.Item label="标题">
+            <Input style={{ width: 400 }} placeholder="请输入文章的标题"></Input>
+          </Form.Item>
+          <Form.Item label="频道">
+            <Channel />
+          </Form.Item>
+          <Form.Item label="封面" name="type">
+            <Radio.Group>
+              <Radio value={1}>单图</Radio>
+              <Radio value={3}>三图</Radio>
+              <Radio value={0}>无图</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+            <Upload listType="picture-card" fileList={fileList}>
+              <PlusOutlined />
+            </Upload>
+          </Form.Item>
+          <Form.Item label="内容" name="content">
+            <ReactQuill></ReactQuill>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 4, span: 20 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                发布文章
+              </Button>
+              <Button>存入草稿</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
